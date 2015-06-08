d53f73bf52d5071c13b3b796df207b04de94eefd
Yiding He <yiding@looppay.com>
2015-06-08 12:58:24 -0400

    Added VolleyUtil

---
+++ app/src/main/java/com/blueskyhe/volleydemo/util/VolleyUtil.java
@@ -0,0 +1,14 @@
+package com.blueskyhe.volleydemo.util;
+
+import android.content.Context;
+
+/**
+ * Created by yiding on 6/8/15.
+ */
+public class VolleyUtil {
+
+    public VolleyUtil(Context context) {
+
+    }
+    
+}
