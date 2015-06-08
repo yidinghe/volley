9e8527a59a03cf321d980f8b6cb30d0c96b30eb9
Yiding He <yiding@looppay.com>
2015-06-08 14:30:39 -0400

    Added stringRequest.

--- app/src/main/java/com/blueskyhe/volleydemo/util/VolleyUtil.java
+++ app/src/main/java/com/blueskyhe/volleydemo/util/VolleyUtil.java
@@ -1,14 +1,44 @@
 package com.blueskyhe.volleydemo.util;
 
 import android.content.Context;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 /**
  * Created by yiding on 6/8/15.
  */
 public class VolleyUtil {
 
-    public VolleyUtil(Context context) {
+    private RequestQueue requestQueue;
 
+    private Context context;
+
+    public VolleyUtil(Context context) {
+        requestQueue = Volley.newRequestQueue(context);
+        this.context = context;
+    }
+
+    public void volleyStringRequest(String url) {
+
+        final String response;
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String s) {
+                Log.d("TAG", s);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError volleyError) {
+                Log.e("TAG", volleyError.getMessage(), volleyError);
+            }
+        });
     }
 
 }
